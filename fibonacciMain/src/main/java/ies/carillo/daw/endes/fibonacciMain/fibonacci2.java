222df5756a10a135b58964763fe53ea76b84d981
ManuRguez <116264970+ManuRguez@users.noreply.github.com>
2022-12-01 10:36:43 +0100

    ProyectoMaven-Eclipse

---
+++ fibonacciMain/src/main/java/ies/carillo/daw/endes/fibonacciMain/fibonacci2.java
@@ -0,0 +1,12 @@
+package ies.carillo.daw.endes.fibonacciMain;
+
+import ies.carillo.daw.endes.fibonacci1.*;
+
+public class fibonacci2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			fibonacci.main(null);
+	}
+
+}
