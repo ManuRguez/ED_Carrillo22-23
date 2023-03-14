838e9fc9f461cc6dcef74ac625bb259910c2dd29
ManuRguez <116264970+ManuRguez@users.noreply.github.com>
2023-03-14 10:04:43 +0100

    PruebaGit

---
+++ Pruebagit/src/main/java/Pruebagit/Pruebagit/App.java
@@ -0,0 +1,13 @@
+package Pruebagit.Pruebagit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
